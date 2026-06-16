15a0ed99928326969cb4501b3ad5123f575afb68
Fezem <lennholz09@gmail.com>
2026-06-16 13:57:07 +0200

    Refactor weapons class to manage equipped weapon globally
    Removed equipped attribute from the weapons class and adjusted equip method to use a global variable for equipped weapon.

--- TB_RPG/Class_Wepons.py
+++ TB_RPG/Class_Wepons.py
@@ -2,16 +2,16 @@ class weapons():#
 
     global equiped_weapon
 
-    def __init__(self, name:str, damage:int, weight:int, equipped:bool=False, durability:int=100):
+    def __init__(self, name:str, damage:int, weight:int, durability:int=100):
         self.name = name
         self.damage = damage
         self.weight = weight
-        self.equipped = equipped
         self.durability = durability
 
     def equip(self):
-        self.equipped = True
-        equiped_weapon = self.name
+        globle equiped_weapon
+        
+        equiped_weapon = self.name  ####################
         print(f"You have equipped {self.name}.")
     def unequip(self):
         self.equipped = False
